c7777950cf7d5e2280aa5b0d4eca55a5febe3c2f
teepunkt-esspunkt <129230571+teepunkt-esspunkt@users.noreply.github.com>
2023-08-09 16:22:37 +0200

    Delete veranstaltungen/ha_1 directory

--- veranstaltungen/ha_1/pindex.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'functions.php';
-
-include TEMPLATES . 'tabellephp.phtml';
--- veranstaltungen/ha_1/veranstaltung_aendern.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'functions.php';
-require_once 'functions_veranstaltungen.php';
-
-
-include TEMPLATES . 'veranstaltung_aendern.phtml';
