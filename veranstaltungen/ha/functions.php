cd37292bddeae772a9e48e0f35053634f2e748a6
23 <23@192.168.160.1>
2023-08-10 22:17:38 +0200

    erweiterung der meineLinkeNavBarLeiste Funktion Externe Links oeffnen nun in neuen Tab

--- veranstaltungen/ha/functions.php
+++ veranstaltungen/ha/functions.php
@@ -80,13 +80,19 @@ function dieDump($varToDump, $title = '', $method = DUMP_VARDUMP)
     die;
 }
 
+
+// Funktion fuer die linke Navigationsleiste, mit IF-Abfrage um Links auf fremde Webseiten in einem neuen Tab zu oeffnen
 function meineLinkeNavBarLeiste(){
     $meineStandardLinkeNavBarDatei = fopen('meineStandardLinkeNavBar.csv', 'r');
     while($meineStandardLinkeNavBarLinks = fgetcsv($meineStandardLinkeNavBarDatei)){
      $meineStandardLinkeNavBarArray[] = $meineStandardLinkeNavBarLinks;
 }
-fclose($meineStandardLinkeNavBarDatei);  
+    fclose($meineStandardLinkeNavBarDatei);  
     foreach($meineStandardLinkeNavBarArray as $key => $value){
+        if(str_contains($meineStandardLinkeNavBarArray[$key][0], 'http://')) {
+            echo  "<li><a href = " . $meineStandardLinkeNavBarArray[$key][0] . ' target="_blank">' . $meineStandardLinkeNavBarArray[$key][1] . "</a></li>";
+        } else {
         echo  "<li><a href = " . $meineStandardLinkeNavBarArray[$key][0] . ">" . $meineStandardLinkeNavBarArray[$key][1] . "</a></li>";
+}
 }
 }
